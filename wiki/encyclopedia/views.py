5e837f41a8813c31d63cde60b4d2a86ebe939fd2
Hashaam Zafar <hashaam1217@gmail.com>
2023-08-20 02:45:32 +0500

    Finished requirement 1 of CS50W project 1

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
-
 from . import util
 
+from django.http import HttpResponse
+import markdown2
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def pages(request, page_name):
+    output = util.get_entry(page_name)
+    
+    if (output is not None):
+        #return HttpResponse(markdown2.markdown(output))
+        return render(request, "encyclopedia/pages.html", {
+            "page_name": page_name,
+            "markdown_to_HTML": markdown2.markdown(output),
+            })
+
+    return render(request, "encyclopedia/pagenotfound.html")
