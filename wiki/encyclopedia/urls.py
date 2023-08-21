16494db4d55c99cdd0e45beb8cae422c84c789a9
Hashaam Zafar <hashaam1217@gmail.com>
2023-08-21 14:26:53 +0500

    Finished New Page and added comments

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:page_name>/", views.pages, name="pages"),
-    path("Search/", views.Search, name="Search")
+    path("Search/", views.Search, name="Search"),
+    path("NewPage/", views.NewPage, name="NewPage")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
 from . import util
 
+from django import forms
 from django.http import HttpResponse
 import markdown2
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
+#For NewPage function for creating a new wiki entry
+class NewTaskForm(forms.Form):
+    title = forms.CharField(label="title")
+    page = forms.CharField(widget=forms.Textarea, label="NewPage")
+
+#Default index page listing all wiki entries
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+#Displays a wiki entry
 def pages(request, page_name):
     output = util.get_entry(page_name)
     
@@ -20,6 +30,7 @@ def pages(request, page_name):
 
     return render(request, "encyclopedia/pagenotfound.html")
 
+#Functionality for the search bar and the search page
 def Search(request):
     query = request.GET.get('q', '')
     if (query in util.list_entries()):
@@ -34,4 +45,45 @@ def Search(request):
     return render(request, "encyclopedia/searchresults.html", {
         "query":query, 
         "entries":searchresults
+        })
+
+def NewPage(request):
+    error = "Page already exists"
+    no_error = " "
+
+    #Handles a POST request
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+
+        #Serverside validation
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            page = form.cleaned_data["page"]
+
+            #Ensures duplicate page doesn't exist and returns the entry back if it is
+            if (title in util.list_entries()):
+                return render(request, "encyclopedia/newpage.html", {
+                    "form":form,
+                    "error":error
+                    })
+
+            #Saves to disk
+            util.save_entry(title, page)
+            #Takes us to a new form
+            return render(request, "encyclopedia/newpage.html", {
+                "form": NewTaskForm(),
+                "error":no_error
+                })
+
+        #If form is not valid, it is simply returned to the user
+        else: 
+            return render(request, "encyclopedia/newpage.html", {
+                "form":form,
+                "error":no_error
+                })
+
+    #If it is a GET request it is taken to a new request
+    return render(request, "encyclopedia/newpage.html", {
+        "form": NewTaskForm(),
+        "error":no_error
         })
